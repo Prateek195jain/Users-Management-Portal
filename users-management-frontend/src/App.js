1cd59f67582b012108ea51ac1f9fb7c3fab4216e
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-11-24 20:18:57 +0530

    frontend

---
+++ users-management-frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/common/Navbar";
+import LoginPage from "./components/auth/LoginPage";
+import RegistrationPage from "./components/auth/RegistrationPage";
+import FooterComponent from "./components/common/Footer";
+import UpdateUser from "./components/usersPage/UpdateUser";
+import ProfilePage from "./components/usersPage/ProfilePage";
+import UserManagementPage from "./components/usersPage/UserManagementPage";
+import UserService from "./components/service/UserService";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            {/* Check if user is authenticated and admin before rendering admin-only routes */}
+            {UserService.adminOnly() && (
+              <>
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route
+                  path="/admin/user-management"
+                  element={<UserManagementPage />}
+                />
+                <Route path="/update-user/:userId" element={<UpdateUser />} />
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
